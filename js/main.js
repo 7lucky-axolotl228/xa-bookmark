36f5ddb7f68bcbe9c3294cc3c9a3f6eab60d92c6
7lucky-axolotl228 <7error228@gmail.com>
2023-07-03 13:38:11 +0500

    Make a function for remove class from elTabsItem

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 const elsTabsItem = document.querySelectorAll('.tabs__item');
 const elsTabLink = document.querySelectorAll('.js-tab-link');
 
+function daectivateTabItems () {
+  elsTabsItem.forEach(function (elTabsItem) {
+    elTabsItem.classList.remove('tabs__item--active');
+  });
+}
+
 elsTabLink.forEach(function (elTabLink) {
   elTabLink.addEventListener('click', function (evt) {
     // Prevent page move
     evt.preventDefault();
 
     // Remove active class from tabs__item elements
-    elsTabsItem.forEach(function (elTabsItem) {
-      elTabsItem.classList.remove('tabs__item--active');
-    });
+    daectivateTabItems();
 
     // Add active class to clicked tabs__item
     elTabLink.parentElement.classList.add('tabs__item--active');
